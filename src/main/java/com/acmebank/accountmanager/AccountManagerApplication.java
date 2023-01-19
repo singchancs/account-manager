f28862588178c3c8edccc5ef5a07af74290f9ef4
Sing Chan <singchancs@gmail.com>
2023-01-19 17:41:37 +0800

    update security settings

--- src/main/java/com/acmebank/accountmanager/AccountManagerApplication.java
+++ src/main/java/com/acmebank/accountmanager/AccountManagerApplication.java
@@ -2,9 +2,8 @@ package com.acmebank.accountmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication()
 public class AccountManagerApplication {
 
 	public static void main(String[] args) {
