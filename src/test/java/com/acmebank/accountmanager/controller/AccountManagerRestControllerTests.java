ecfbeb865dc593c9f1bb5a0d816eb76b9f22f89e
Sing Chan <singchancs@gmail.com>
2023-01-22 00:59:22 +0800

    refactor unit test

--- src/test/java/com/acmebank/accountmanager/controller/AccountManagerRestControllerTests.java
+++ src/test/java/com/acmebank/accountmanager/controller/AccountManagerRestControllerTests.java
@@ -2,7 +2,6 @@ package com.acmebank.accountmanager.controller;
 
 import com.acmebank.accountmanager.dao.AccountRepository;
 import com.acmebank.accountmanager.model.Account;
-import org.junit.Before;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +9,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -35,33 +32,39 @@ public class AccountManagerRestControllerTests {
         repository.save(account);
     }
 
-    public void resetBalance() {
+    private void resetBalance() {
         resetAccountBalance("12345678", "1000000");
         resetAccountBalance("88888888", "1000000");
+    }
+
+    private HttpHeaders getAccountHeader(String accountNumber) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Account-Number", accountNumber);
+        return httpHeaders;
     }
 
     @Test
     @DisplayName("Should return balance when request is valid")
     public void shouldReturnBalanceWhenValid() throws Exception {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Account-Number","12345678");
+        this.resetBalance();
+
         mvc.perform(MockMvcRequestBuilders.get("/api/1.0/accountmanager/balance")
                         .accept(MediaType.APPLICATION_JSON)
-                        .headers(httpHeaders)
+                        .headers(this.getAccountHeader("12345678"))
                 )
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.currency").value("HKD"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.balance").isNumber())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.balance").value("1000000.0000"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.lastUpdatedAt").isNotEmpty());
     }
     @Test
     @DisplayName("Should return 404 when account is not found")
     public void shouldReturn404WhenAccountNotFound() throws Exception {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Account-Number","00000000");
+        this.resetBalance();
+
         mvc.perform(MockMvcRequestBuilders.get("/api/1.0/accountmanager/balance")
                         .accept(MediaType.APPLICATION_JSON)
-                        .headers(httpHeaders)
+                        .headers(this.getAccountHeader("00000000"))
                 )
                 .andExpect(status().isNotFound())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.apierror.errorCode").value("ERR_ACME_002"));;
@@ -71,35 +74,34 @@ public class AccountManagerRestControllerTests {
     @DisplayName("Should transfer to account when request is valid")
     public void shouldTransferWhenValid() throws Exception {
         this.resetBalance();
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Account-Number","12345678");
+
         mvc.perform(MockMvcRequestBuilders.post("/api/1.0/accountmanager/transfer")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
-                .headers(httpHeaders)
-                .content("{ \"targetAccountNumber\": \"88888888\", \"transferCurrency\": \"HKD\", \"transferAmount\": \"5000.0\"}")
+                .headers(this.getAccountHeader("12345678"))
+                .content("{ \"targetAccountNumber\": \"88888888\", \"transferCurrency\": \"HKD\", \"transferAmount\": \"500000.0000\"}")
         )
                 .andExpect(status().isCreated())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("success"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.currency").value("HKD"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.amount").value(5000))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.amount").value("500000.0000"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.createdAt").isNotEmpty())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.transferAt").isNotEmpty())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.transferTo").value("88888888"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.balance.currency").value("HKD"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.balance.balance").isNumber())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.balance.balance").value("500000.0000"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.balance.lastUpdatedAt").isNotEmpty());
     }
 
     @Test
     @DisplayName("Should return 404 when source account is not found")
     public void shouldReturn404WhenSourceAccountNotFound() throws Exception {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Account-Number","11111111");
+        this.resetBalance();
+
         mvc.perform(MockMvcRequestBuilders.post("/api/1.0/accountmanager/transfer")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
-                .headers(httpHeaders)
+                .headers(this.getAccountHeader("11111111"))
                 .content("{ \"targetAccountNumber\": \"88888888\", \"transferCurrency\": \"HKD\", \"transferAmount\": \"5000.0\"}")
         )
                 .andExpect(status().isNotFound())
@@ -109,12 +111,12 @@ public class AccountManagerRestControllerTests {
     @Test
     @DisplayName("Should return 404 when target account is not found")
     public void shouldReturn404WhenTargetAccountNotFound() throws Exception {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Account-Number","12345678");
+        this.resetBalance();
+
         mvc.perform(MockMvcRequestBuilders.post("/api/1.0/accountmanager/transfer")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
-                .headers(httpHeaders)
+                .headers(this.getAccountHeader("12345678"))
                 .content("{ \"targetAccountNumber\": \"22222222\", \"transferCurrency\": \"HKD\", \"transferAmount\": \"5000.0\"}")
         ).andExpect(status().isNotFound())
         .andExpect(MockMvcResultMatchers.jsonPath("$.apierror.errorCode").value("ERR_ACME_002"));
@@ -123,12 +125,12 @@ public class AccountManagerRestControllerTests {
     @Test
     @DisplayName("Should return 500 when source account is the same as target account ")
     public void shouldReturn500WhenSourceTargetAreSame() throws Exception {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Account-Number","12345678");
+        this.resetBalance();
+
         mvc.perform(MockMvcRequestBuilders.post("/api/1.0/accountmanager/transfer")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
-                .headers(httpHeaders)
+                .headers(this.getAccountHeader("12345678"))
                 .content("{ \"targetAccountNumber\": \"12345678\", \"transferCurrency\": \"HKD\", \"transferAmount\": \"5000.0\"}")
         )
                 .andExpect(status().isInternalServerError())
@@ -140,12 +142,11 @@ public class AccountManagerRestControllerTests {
     public void shouldReturn500WhenInsufficientBalance() throws Exception {
         this.resetBalance();
         this.resetAccountBalance("12345678", "1000.0000");
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Account-Number","12345678");
+
         mvc.perform(MockMvcRequestBuilders.post("/api/1.0/accountmanager/transfer")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
-                .headers(httpHeaders)
+                .headers(this.getAccountHeader("12345678"))
                 .content("{ \"targetAccountNumber\": \"88888888\", \"transferCurrency\": \"HKD\", \"transferAmount\": \"5000.0\"}")
         )
                 .andExpect(status().isInternalServerError())
@@ -157,12 +158,11 @@ public class AccountManagerRestControllerTests {
     public void shouldReturn500WhenZeroBalance() throws Exception {
         this.resetBalance();
         this.resetAccountBalance("12345678", "0.0000");
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Account-Number","12345678");
+
         mvc.perform(MockMvcRequestBuilders.post("/api/1.0/accountmanager/transfer")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
-                .headers(httpHeaders)
+                .headers(this.getAccountHeader("12345678"))
                 .content("{ \"targetAccountNumber\": \"88888888\", \"transferCurrency\": \"HKD\", \"transferAmount\": \"5000.0\"}")
         )
                 .andExpect(status().isInternalServerError())
@@ -172,12 +172,12 @@ public class AccountManagerRestControllerTests {
     @Test
     @DisplayName("Should return 400 when incorrect parameters supplied")
     public void shouldReturn500WhenIncorrectParamSupplied() throws Exception {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Account-Number","12345678");
+        this.resetBalance();
+
         mvc.perform(MockMvcRequestBuilders.post("/api/1.0/accountmanager/transfer")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
-                .headers(httpHeaders)
+                .headers(this.getAccountHeader("12345678"))
                 .content("{ \"targetAccountNumber\": \"88888888\", \"transferCurrency\": \"HKD\" }")
         )
                 .andExpect(status().isBadRequest())
