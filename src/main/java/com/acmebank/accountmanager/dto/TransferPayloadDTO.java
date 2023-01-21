4343cd23c47b912e4bd6f130654037d6d4a04fe6
Sing Chan <singchancs@gmail.com>
2023-01-21 11:50:11 +0800

    bug fix for import

--- src/main/java/com/acmebank/accountmanager/dto/TransferPayloadDTO.java
+++ src/main/java/com/acmebank/accountmanager/dto/TransferPayloadDTO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import org.hibernate.validator.constraints.NotBlank;
 
 import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
